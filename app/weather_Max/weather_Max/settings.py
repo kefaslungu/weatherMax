c1af010124903be3d1d9da3d1d351d2f404131fb
kefaslungu <jameskefaslungu@gmail.com>
2023-09-22 07:59:41 +0100

    changedUrls

--- app/weather_Max/weather_Max/settings.py
+++ app/weather_Max/weather_Max/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gg-ui70x_#k$9ec5go@fx!44!z6a=gio2t!1b^oc--cb@iw-x('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
